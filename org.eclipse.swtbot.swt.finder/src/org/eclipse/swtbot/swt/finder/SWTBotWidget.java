822e90d05fa64700a8a029e4afd9a5e1ec27bbaf
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-01-01 08:11:56 +0000

    improved javadoc for the annotation class

--- org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/SWTBotWidget.java
+++ org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/SWTBotWidget.java
@@ -28,7 +28,50 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Widget;
 
 /**
- * Marks a SWTBot widget so tools recognise them.
+ * Marks a SWTBot widget so tools recognise them. This anotation is primarily used to describe the convinience API that
+ * should be generated for a class annotated with the specified widget.
+ * <p>
+ * Usage:
+ * 
+ * <pre>
+ * &#064;SWTBotWidget(clasz = Button.class, style = @Style(name = &quot;SWT.PUSH&quot;, value = SWT.PUSH), preferredName = &quot;button&quot;, referenceBy = { ReferenceBy.LABEL, ReferenceBy.MNEMONIC, ReferenceBy.TOOLTIP })
+ * public class SWTBotButton ...{
+ *   // the implement 
+ * }
+ * </pre>
+ * 
+ * The above represents:
+ * <ul>
+ * <li>widget of type 'Button.class'</li>
+ * <li>with style bits 'SWT.PUSH'</li>
+ * <li>the preferred name for the generated API would be 'button'</li>
+ * <li>the widget can be referenced by: <strong>a combination</strong> of LABEL, MNEMONIC, TOOLTIP, in addition to the
+ * defaults described in {@link #defaultReferenceBy()}</li>
+ * </ul>
+ * This annotation will generate the following convinience API to find buttons:
+ * <ul>
+ * <li>public SWTBotButton buttonWithLabel(String label)</li>
+ * <li>public SWTBotButton buttonWithLabel(String label, int index)</li>
+ * <li>public SWTBotButton button(String mnemonicText)</li>
+ * <li>public SWTBotButton button(String mnemonicText, int index)</li>
+ * <li>public SWTBotButton buttonWithTooltip(String tooltip)</li>
+ * <li>public SWTBotButton buttonWithTooltip(String tooltip, int index)</li>
+ * <li>public SWTBotButton buttonWithId(String key, String value)</li>
+ * <li>public SWTBotButton buttonWithId(String key, String value, int index)</li>
+ * <li>public SWTBotButton buttonWithId(String value)</li>
+ * <li>public SWTBotButton buttonWithId(String value, int index)</li>
+ * <li>public SWTBotButton buttonInGroup(String inGroup)</li>
+ * <li>public SWTBotButton buttonInGroup(String inGroup, int index)</li>
+ * <li>public SWTBotButton button()</li>
+ * <li>public SWTBotButton button(int index)</li>
+ * <li>public SWTBotButton buttonWithLabelInGroup(String label, String inGroup)</li>
+ * <li>public SWTBotButton buttonWithLabelInGroup(String label, String inGroup, int index)</li>
+ * <li>public SWTBotButton buttonInGroup(String mnemonicText, String inGroup)</li>
+ * <li>public SWTBotButton buttonInGroup(String mnemonicText, String inGroup, int index)</li>
+ * <li>public SWTBotButton buttonWithTooltipInGroup(String tooltip, String inGroup)</li>
+ * <li>public SWTBotButton buttonWithTooltipInGroup(String tooltip, String inGroup, int index)</li>
+ * </ul>
+ * </p>
  * 
  * @author Ketan Padegaonkar &lt;KetanPadegaonkar [at] gmail [dot] com&gt;
  * @version $Id$
