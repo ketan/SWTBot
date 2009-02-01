123b224043e8ff34dc520fe815b12b37a5adc1c0
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-02-01 10:32:58 +0000

    added better matchers for workbench parts

--- org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/matchers/WidgetMatcherFactory.java
+++ org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/matchers/WidgetMatcherFactory.java
@@ -14,4 +14,15 @@ public abstract class WidgetMatcherFactory extends org.eclipse.swtbot.swt.finder
     return org.eclipse.swtbot.eclipse.finder.matchers.WithPartName.withPartName(text);
   }
 
+  /**
+   * Matches a workbench part (view/editor) with the specified name.
+   * 
+   * @param nameMatcher the part name matcher.
+   * @return a matcher.
+   * @since 2.0
+   */
+  public static org.hamcrest.Matcher<? extends org.eclipse.ui.IWorkbenchPartReference> withPartName(org.hamcrest.Matcher<java.lang.String> nameMatcher) {
+    return org.eclipse.swtbot.eclipse.finder.matchers.WithPartName.withPartName(nameMatcher);
+  }
+
 }
--- org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/matchers/WithPartName.java
+++ org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/matchers/WithPartName.java
@@ -8,41 +8,46 @@
  * Contributors:
  *     Ketan Padegaonkar - initial API and implementation
  *     Ketan Padegaonkar - http://swtbot.org/bugzilla/show_bug.cgi?id=126
+ *     Ketan Patel (bug 260088)
  *******************************************************************************/
 package org.eclipse.swtbot.eclipse.finder.matchers;
+
+import static org.hamcrest.Matchers.equalTo;
 
 import org.eclipse.swtbot.swt.finder.matchers.AbstractMatcher;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
 
 
 /**
  * @author Ketan Padegaonkar &lt;KetanPadegaonkar [at] gmail [dot] com&gt;
+ * @author Ketan Patel
  * @version $Id$
  * @since 2.0
  */
 public class WithPartName<T extends IWorkbenchPartReference> extends AbstractMatcher<T> {
 
-	private final String	viewLabel;
+	private final Matcher<String> nameMatcher;
 
 	/**
-	 * @param partName the label of the view.
+	 * @param nameMatcher the part name matcher.
 	 */
-	public WithPartName(String partName) {
-		this.viewLabel = partName;
+	public WithPartName(Matcher<String> nameMatcher) {
+		this.nameMatcher = nameMatcher;
 	}
 
 	public boolean doMatch(Object item) {
 		if (item instanceof IWorkbenchPartReference) {
 			IWorkbenchPartReference part = (IWorkbenchPartReference) item;
-			return part.getPartName().equals(viewLabel);
+			return nameMatcher.matches(part.getPartName());
 		}
 		return false;
 	}
 
 	public void describeTo(Description description) {
-
+		description.appendText("with name '").appendDescriptionOf(nameMatcher).appendText("'");
 	}
 
 	/**
@@ -54,7 +59,18 @@ public class WithPartName<T extends IWorkbenchPartReference> extends AbstractMat
 	 */
 	@Factory
 	public static AbstractMatcher<? extends IWorkbenchPartReference> withPartName(String text) {
-		return new WithPartName<IWorkbenchPartReference>(text);
+		return withPartName(equalTo(text));
 	}
 
+	/**
+	 * Matches a workbench part (view/editor) with the specified name.
+	 *
+	 * @param nameMatcher the part name matcher.
+	 * @return a matcher.
+	 * @since 2.0
+	 */
+	@Factory
+	public static AbstractMatcher<? extends IWorkbenchPartReference> withPartName(Matcher<String> nameMatcher) {
+		return new WithPartName<IWorkbenchPartReference>(nameMatcher);
+	}
 }
