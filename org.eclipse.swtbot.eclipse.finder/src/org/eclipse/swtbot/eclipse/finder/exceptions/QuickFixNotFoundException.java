16ee30ab009e35291766f80dc71fa3ca64c7b021
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-05-13 12:01:36 +0000

    changed QFNFE to a runtime exception

--- org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/exceptions/QuickFixNotFoundException.java
+++ org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/exceptions/QuickFixNotFoundException.java
@@ -14,7 +14,7 @@ package org.eclipse.swtbot.eclipse.finder.exceptions;
  * @author Ketan Padegaonkar &lt;KetanPadegaonkar [at] gmail [dot] com&gt;
  * @version $Id$
  */
-public class QuickFixNotFoundException extends Exception {
+public class QuickFixNotFoundException extends RuntimeException {
 	/**
 	 * The serialization version identifier.
 	 */
