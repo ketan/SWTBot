1eb0f2de452ea28db9c92b1a5b480890b6957f1a
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-05-21 07:32:20 +0000

    changed visibility of constants.

--- org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/utils/SWTBotPreferences.java
+++ org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/utils/SWTBotPreferences.java
@@ -27,20 +27,29 @@ import org.eclipse.swtbot.swt.finder.keyboard.KeyboardStrategy;
  * @since 1.1
  */
 public class SWTBotPreferences {
-	private static final String	KEY_DEFAULT_KEY					= "org.eclipse.swtbot.search.defaultKey";
-	private static final String	KEY_TIMEOUT						= "org.eclipse.swtbot.search.timeout";
-	
-	private static final String	KEY_RECORER_FILE_NAME			= "org.eclipse.swtbot.recorder.file.name";
-	
-	private static final String	KEY_PLAYBACK_DELAY				= "org.eclipse.swtbot.playback.delay";
-	
-	private static final String	KEY_MAX_ERROR_SCREENSHOT_COUNT	= "org.eclipse.swtbot.screenshots.error.maxcount";
-	private static final String	KEY_SCREENSHOTS_DIR				= "org.eclipse.swtbot.screenshots.dir";
-	private static final String	KEY_SCREENSHOT_FORMAT			= "org.eclipse.swtbot.screenshots.format";
-	
-	private static final String	KEY_KEYBOARD_LAYOUT				= "org.eclipse.swtbot.keyboard.layout";
-	private static final String	KEY_TYPE_INTERVAL				= "org.eclipse.swtbot.keyboard.interval";
-	private static final String	KEY_KEYBOARD_STRATEGY			= "org.eclipse.swtbot.keyboard.strategy";
+	/** @see #DEFAULT_KEY */
+	public static final String	KEY_DEFAULT_KEY					= "org.eclipse.swtbot.search.defaultKey";
+	/** @see #TIMEOUT */
+	public static final String	KEY_TIMEOUT						= "org.eclipse.swtbot.search.timeout";
+	/** @see #RECORDER_FILE_NAME */
+	public static final String	KEY_RECORER_FILE_NAME			= "org.eclipse.swtbot.recorder.file.name";
+
+	/** @see #PLAYBACK_DELAY */
+	public static final String	KEY_PLAYBACK_DELAY				= "org.eclipse.swtbot.playback.delay";
+
+	/** @see #MAX_ERROR_SCREENSHOT_COUNT */
+	public static final String	KEY_MAX_ERROR_SCREENSHOT_COUNT	= "org.eclipse.swtbot.screenshots.error.maxcount";
+	/** @see #SCREENSHOTS_DIR */
+	public static final String	KEY_SCREENSHOTS_DIR				= "org.eclipse.swtbot.screenshots.dir";
+	/** @see #SCREENSHOT_FORMAT */
+	public static final String	KEY_SCREENSHOT_FORMAT			= "org.eclipse.swtbot.screenshots.format";
+
+	/** @see #KEYBOARD_LAYOUT */
+	public static final String	KEY_KEYBOARD_LAYOUT				= "org.eclipse.swtbot.keyboard.layout";
+	/** @see #TYPE_INTERVAL */
+	public static final String	KEY_TYPE_INTERVAL				= "org.eclipse.swtbot.keyboard.interval";
+	/** @see #KEYBOARD_STRATEGY */
+	public static final String	KEY_KEYBOARD_STRATEGY			= "org.eclipse.swtbot.keyboard.strategy";
 
 	/**
 	 * The default key used to match SWT widgets. Defaults to {@code org.eclipse.swtbot.widget.key}. To set another
@@ -68,7 +77,7 @@ public class SWTBotPreferences {
 
 	/**
 	 * The maximum number of screenshots that SWTBot should capture. Defaults to 100. To set another default use the
-	 * system property {@value #MAX_ERROR_SCREENSHOT_COUNT}.
+	 * system property {@value #KEY_MAX_ERROR_SCREENSHOT_COUNT}.
 	 */
 	public static int			MAX_ERROR_SCREENSHOT_COUNT		= toInt(System.getProperty(KEY_MAX_ERROR_SCREENSHOT_COUNT, "100"), 100);
 
