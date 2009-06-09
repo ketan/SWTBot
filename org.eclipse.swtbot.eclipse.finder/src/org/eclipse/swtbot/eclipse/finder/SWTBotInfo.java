bf8984bab7766b590b9277e61e6e784cb1609df7
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-06-09 16:34:43 +0000

    RESOLVED - 279517: Provide a way of identifying SWTBot versions https://bugs.eclipse.org/bugs/show_bug.cgi?id=279517

---
+++ org.eclipse.swtbot.eclipse.finder/src/org/eclipse/swtbot/eclipse/finder/SWTBotInfo.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Ketan Padegaonkar and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ketan Padegaonkar - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.eclipse.finder;
+
+import org.eclipse.swt.SWT;
+
+/**
+ * Dumps information useful for providing debug info.
+ * 
+ * @author Ketan Padegaonkar &lt;KetanPadegaonkar [at] gmail [dot] com&gt;
+ * @version $Id$
+ */
+public class SWTBotInfo {
+
+	private static final String	VERSION	= "@svn.version@";
+
+	/**
+	 * Prints out some platform specific information useful for diagnosis.
+	 */
+	public static void printInfo() {
+		System.out.println("--- Begin SWTBot Info ---");
+		System.out.println("SWTBot version: " + VERSION);
+		System.out.println("SWT Version: " + SWT.getVersion());
+		System.out.println("SWT Platform: " + SWT.getPlatform());
+		System.out.println("Operating System: " + System.getProperty("os.name") + "/" + System.getProperty("os.arch"));
+		System.out.println("Version " + System.getProperty("os.version"));
+		System.out.println("Operating System: " + System.getProperty("os.arch"));
+		System.out.println("--- End SWTBot Info ---");
+	}
+
+}
