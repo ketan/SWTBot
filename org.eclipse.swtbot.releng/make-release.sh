bb72e1eb4d2affaaaa2ed223238a07585c319338
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-07-06 18:59:49 +0000

    RESOLVED - 282334: Updating properties files for all hosts and howto
    unable to install SWTbot using old UM‎
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=282334

--- org.eclipse.swtbot.releng/make-release.sh
+++ org.eclipse.swtbot.releng/make-release.sh
@@ -5,15 +5,13 @@ rm -rf artifacts target
 # usage: build_swtbot 3.5M6 galileo
 function build_swtbot(){
 	ant materialize-workspace \
-		"-Dupdate.site.features.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site/features" \
-		"-Dupdate.site.plugins.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site/plugins" \
+		"-Dupdate.site.base.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site" \
 		"-Declipse.sdk.archive=eclipse-SDK-$1-macosx-carbon.tar.gz" \
 		"-Declipse.buildId=$1" \
 		"-Dhas.archives=true"
 
 	ant cruise \
-		"-Dupdate.site.features.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site/features" \
-		"-Dupdate.site.plugins.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site/plugins" \
+		"-Dupdate.site.base.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site" \
 		"-Declipse.sdk.archive=eclipse-SDK-$1-macosx-carbon.tar.gz" \
 		"-Declipse.buildId=$1" \
 		"-Dhas.archives=true"
@@ -23,7 +21,7 @@ function build_swtbot(){
 	mv artifacts/to-upload to-upload/$2/dev-build
 }
 
-# build_swtbot 3.4.2 ganymede
-build_swtbot 3.5RC4 galileo
+build_swtbot 3.4.2 ganymede
+build_swtbot 3.5 galileo
 
 # rsync --delete-after --partial --progress --archive to-upload build.eclipse.org:
