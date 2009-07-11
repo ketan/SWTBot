7d67108438f826d8686fd0fb8d09d0642419d11a
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-07-11 07:44:45 +0000

    RESOLVED - 281930: Added an -e34/-e35 suffix to the version numbers.
    updating from Ganymede to Galileo version fails
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=281930

--- org.eclipse.swtbot.releng/make-release.sh
+++ org.eclipse.swtbot.releng/make-release.sh
@@ -2,26 +2,30 @@
 
 rm -rf artifacts target
 
-# usage: build_swtbot 3.5M6 galileo
+# usage: build_swtbot <eclipse-version> <eclipse-major.minor-version> <eclipse-release-name>
+# e.g.: build_swtbot 3.5.1 35 galileo
+# e.g.: build_swtbot 3.4 34 ganymede
 function build_swtbot(){
 	ant materialize-workspace \
-		"-Dupdate.site.base.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site" \
+		"-Dupdate.site.base.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$3/dev-build/update-site" \
 		"-Declipse.sdk.archive=eclipse-SDK-$1-macosx-carbon.tar.gz" \
 		"-Declipse.buildId=$1" \
-		"-Dhas.archives=true"
+		"-Dhas.archives=true" \
+		"-Declipse.qualifier=$2"
 
 	ant cruise \
-		"-Dupdate.site.base.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$2/dev-build/update-site" \
+		"-Dupdate.site.base.url=http://www.eclipse.org/downloads/download.php?r=1&amp;file=/technology/swtbot/$3/dev-build/update-site" \
 		"-Declipse.sdk.archive=eclipse-SDK-$1-macosx-carbon.tar.gz" \
 		"-Declipse.buildId=$1" \
-		"-Dhas.archives=true"
+		"-Dhas.archives=true" \
+		"-Declipse.qualifier=$2"
 
-	rm -rf to-upload/$2
-	mkdir to-upload/$2
-	mv artifacts/to-upload to-upload/$2/dev-build
+	rm -rf to-upload/$3
+	mkdir to-upload/$3
+	mv artifacts/to-upload to-upload/$3/dev-build
 }
 
-build_swtbot 3.4.2 ganymede
-build_swtbot 3.5 galileo
+build_swtbot 3.4.2 e34 ganymede
+build_swtbot 3.5 e35 galileo
 
 # rsync --delete-after --partial --progress --archive to-upload build.eclipse.org:
