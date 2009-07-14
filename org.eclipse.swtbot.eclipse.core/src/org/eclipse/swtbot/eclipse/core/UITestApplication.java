769b9f3216e3fcef62d0b8cae952a4e104947ca8
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-07-14 06:26:43 +0000

    RESOLVED - 283370: Also removed all references to and support for legacy IPlatformRunnable.
    provide a way to launch a custom application/product from library.xml
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=283370

--- org.eclipse.swtbot.eclipse.core/src/org/eclipse/swtbot/eclipse/core/UITestApplication.java
+++ org.eclipse.swtbot.eclipse.core/src/org/eclipse/swtbot/eclipse/core/UITestApplication.java
@@ -11,12 +11,12 @@
  *******************************************************************************/
 package org.eclipse.swtbot.eclipse.core;
 
+import java.util.Arrays;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IPlatformRunnable;
 import org.eclipse.core.runtime.IProduct;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
@@ -26,18 +26,18 @@ import org.eclipse.ui.testing.ITestHarness;
 import org.eclipse.ui.testing.TestableObject;
 
 /**
- * Just a copy of {@link org.eclipse.pde.internal.junit.runtime.UITestApplication} with
- * {@link #runTests()} overridden so that the tests run in a non-UI thread.
+ * Just a copy of {@link org.eclipse.pde.internal.junit.runtime.UITestApplication} with {@link #runTests()} overridden
+ * so that the tests run in a non-UI thread.
+ *
  * @author Ketan Padegaonkar &lt;KetanPadegaonkar [at] gmail [dot] com&gt;
  * @version $Id$
  */
-@SuppressWarnings("all")
 public class UITestApplication implements IApplication, ITestHarness {
 
-	private static final String DEFAULT_APP_3_0 = "org.eclipse.ui.ide.workbench"; //$NON-NLS-1$
+	private static final String	DEFAULT_APP_3_0	= "org.eclipse.ui.ide.workbench";	//$NON-NLS-1$
 
-	private TestableObject fTestableObject;
-	private IApplication fApplication;
+	private TestableObject		fTestableObject;
+	private IApplication		fApplication;
 
 	/*
 	 * (non-Javadoc)
@@ -55,7 +55,7 @@ public class UITestApplication implements IApplication, ITestHarness {
 			fApplication = (IApplication) app;
 			return fApplication.start(context);
 		}
-		return ((IPlatformRunnable) app).run(args);
+		throw new IllegalArgumentException("Could not execute application " + getApplicationToRun(args));
 	}
 
 	/*
@@ -68,18 +68,14 @@ public class UITestApplication implements IApplication, ITestHarness {
 	}
 
 	/*
-	 * return the application to run, or null if not even the default application
-	 * is found.
+	 * return the application to run, or null if not even the default application is found.
 	 */
 	private Object getApplication(String[] args) throws CoreException {
 		// Find the name of the application as specified by the PDE JUnit launcher.
 		// If no application is specified, the 3.0 default workbench application
 		// is returned.
-		IExtension extension =
-			Platform.getExtensionRegistry().getExtension(
-					Platform.PI_RUNTIME,
-					Platform.PT_APPLICATIONS,
-					getApplicationToRun(args));
+		IExtension extension = Platform.getExtensionRegistry().getExtension(Platform.PI_RUNTIME, Platform.PT_APPLICATIONS,
+				getApplicationToRun(args));
 
 		Assert.isNotNull(extension);
 
@@ -90,7 +86,7 @@ public class UITestApplication implements IApplication, ITestHarness {
 			IConfigurationElement[] runs = elements[0].getChildren("run"); //$NON-NLS-1$
 			if (runs.length > 0) {
 				Object runnable = runs[0].createExecutableExtension("class"); //$NON-NLS-1$
-				if ((runnable instanceof IPlatformRunnable) || (runnable instanceof IApplication))
+				if (runnable instanceof IApplication)
 					return runnable;
 			}
 		}
@@ -98,19 +94,17 @@ public class UITestApplication implements IApplication, ITestHarness {
 	}
 
 	/*
-	 * The -testApplication argument specifies the application to be run.
-	 * If the PDE JUnit launcher did not set this argument, then return
-	 * the name of the default application.
-	 * In 3.0, the default is the "org.eclipse.ui.ide.worbench" application.
-	 *
+	 * The -testApplication argument specifies the application to be run. If the PDE JUnit launcher did not set this
+	 * argument, then return the name of the default application. In 3.0, the default is the
+	 * "org.eclipse.ui.ide.worbench" application.
 	 */
 	private String getApplicationToRun(String[] args) {
 		IProduct product = Platform.getProduct();
 		if (product != null)
 			return product.getApplication();
 		for (int i = 0; i < args.length; i++)
-			if (args[i].equals("-testApplication") && (i < args.length -1)) //$NON-NLS-1$
-				return args[i+1];
+			if (args[i].equals("-testApplication") && (i < args.length - 1)) //$NON-NLS-1$
+				return args[i + 1];
 		return DEFAULT_APP_3_0;
 	}
 
