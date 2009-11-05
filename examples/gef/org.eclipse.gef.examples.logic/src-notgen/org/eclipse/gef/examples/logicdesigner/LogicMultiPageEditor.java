f469cf30adfaf5245afe4d71ee9dcd8d58e47fb7
Mariot Chauvin <mariot.chauvin@obeo.fr>
2009-11-05 17:24:40 +0000

    add multiple part editor example

--- examples/gef/org.eclipse.gef.examples.logic/src-notgen/org/eclipse/gef/examples/logicdesigner/LogicMultiPageEditor.java
+++ examples/gef/org.eclipse.gef.examples.logic/src-notgen/org/eclipse/gef/examples/logicdesigner/LogicMultiPageEditor.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Obeo
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mariot Chauvin <mariot.chauvin@obeo.fr> - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.gef.examples.logicdesigner;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -10,7 +21,6 @@ public class LogicMultiPageEditor extends MultiPageEditorPart {
 	
 	private LogicEditor editor2;
 	
-	@Override
 	protected void createPages() {
 		editor1 = new LogicEditor();
 		editor2 = new LogicEditor();
@@ -22,19 +32,17 @@ public class LogicMultiPageEditor extends MultiPageEditorPart {
 		}
 	}
 
-	@Override
+	
 	public void doSave(IProgressMonitor monitor) {
 		editor1.doSave(monitor);
 		editor2.doSave(monitor);
 	}
 
-	@Override
 	public void doSaveAs() {
 		editor1.doSaveAs();
 		editor2.doSaveAs();
 	}
 
-	@Override
 	public boolean isSaveAsAllowed() {
 		return editor1.isSaveAsAllowed() && editor2.isSaveAsAllowed();
 	}
