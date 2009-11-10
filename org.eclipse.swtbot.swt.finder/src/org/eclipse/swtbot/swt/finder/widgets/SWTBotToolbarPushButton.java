4c6593b4f7681322b136d507124f5cf45a4aa52c
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2009-11-10 05:24:00 +0000

    added a source file that I missed as part of 293937, r431.

---
+++ org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/widgets/SWTBotToolbarPushButton.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Obeo and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.swt.finder.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swtbot.swt.finder.ReferenceBy;
+import org.eclipse.swtbot.swt.finder.SWTBotWidget;
+import org.eclipse.swtbot.swt.finder.Style;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.utils.MessageFormat;
+import org.eclipse.swtbot.swt.finder.utils.SWTUtils;
+import org.eclipse.swtbot.swt.finder.utils.internal.Assert;
+import org.hamcrest.SelfDescribing;
+
+/**
+ * @author Mariot Chauvin &lt;mariot [dot] chauvin [at] obeo [dot] fr&gt;
+ */
+@SWTBotWidget(clasz = ToolItem.class, style = @Style(name="SWT.PUSH", value=SWT.PUSH), preferredName = "toolbarButton", referenceBy = { ReferenceBy.MNEMONIC, ReferenceBy.TOOLTIP })
+public class SWTBotToolbarPushButton extends SWTBotToolbarButton {
+
+	/**
+	 * Constructs an new instance of this item.
+	 *
+	 * @param w the tool item.
+	 * @throws WidgetNotFoundException if the widget is <code>null</code> or widget has been disposed.
+	 */
+	public SWTBotToolbarPushButton(ToolItem w) throws WidgetNotFoundException {
+		this(w, null);
+	}
+
+	/**
+	 * Constructs an new instance of this item.
+	 *
+	 * @param w the tool item.
+	 * @param description the description of the widget, this will be reported by {@link #toString()}
+	 * @throws WidgetNotFoundException if the widget is <code>null</code> or widget has been disposed.
+	 */
+	public SWTBotToolbarPushButton(ToolItem w, SelfDescribing description) throws WidgetNotFoundException {
+		super(w, description);
+		Assert.isTrue(SWTUtils.hasStyle(w, SWT.PUSH), "Expecting a push button."); //$NON-NLS-1$
+	}
+
+	/**
+	 * Click on the tool item.
+	 *
+	 * @since 1.0
+	 */
+	public SWTBotToolbarPushButton click() {
+		log.debug(MessageFormat.format("Clicking on {0}", this)); //$NON-NLS-1$
+		assertEnabled();
+		sendNotifications();
+		log.debug(MessageFormat.format("Clicked on {0}", this)); //$NON-NLS-1$
+		return this;
+	}
+}
