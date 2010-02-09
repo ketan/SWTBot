a55bd64130785d926ee7a2684ecfae9113e4fc69
Mariot Chauvin <mariot.chauvin@obeo.fr>
2010-02-09 14:42:11 +0000

    repair logic editor and unit tests

--- examples/gef/org.eclipse.gef.examples.logic.test/src/org/eclipse/gef/examples/logic/test/unit/AllTests.java
+++ examples/gef/org.eclipse.gef.examples.logic.test/src/org/eclipse/gef/examples/logic/test/unit/AllTests.java
@@ -11,32 +11,43 @@
 
 package org.eclipse.gef.examples.logic.test.unit;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.examples.logic.test.CreateLogicDiagram;
 import org.eclipse.gef.examples.logic.test.NewEmptyEmfProject;
+import org.eclipse.gef.examples.logicdesigner.edit.LogicLabelEditPart;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 
 public class AllTests extends SWTBotGefForUnitTestsTestCase {
 
-
+	private static final String PROJECT_NAME= "Test";
+	
+	private static final String FILE_NAME= "test.logic";
+	
 	private NewEmptyEmfProject emfProject = new NewEmptyEmfProject();
 	
 	private CreateLogicDiagram logicDiagram = new CreateLogicDiagram();
 
 	private SWTBotGefForUnitTestsEditor editor;
 	
-
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		closeWelcomePage();
-		emfProject.createProject("test");
-		logicDiagram.createFile("test", "test.logic");
-		editor = bot.gefEditor("test.logic");
+		emfProject.createProject(PROJECT_NAME);
+		logicDiagram.createFile(PROJECT_NAME, FILE_NAME);
+		editor = bot.gefEditor(FILE_NAME);
 	}
 
 	@Override
-	protected void tearDown() throws Exception {
-		saveCurrentEditor();
+	protected void tearDown() throws Exception {		
+		//saveCurrentEditor();
+		editor.close();
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJECT_NAME);
+		project.delete(true, null);
 		super.tearDown();
 	}
 
@@ -54,11 +65,58 @@ public class AllTests extends SWTBotGefForUnitTestsTestCase {
 	
 	public void testActivateTool() {	
 		editor.activateTool("Circuit");
-		editor.forTestGetEditDomain().getActiveTool();
+		assertEquals("Circuit", getActiveToolLabel());
 		
+		editor.activateTool("Connection");
+		assertEquals("Connection", getActiveToolLabel());
+		
+		editor.activateTool("Or Gate");
+		assertEquals("Or Gate", getActiveToolLabel());
+
+		editor.activateTool("Circuit");
+		assertEquals("Circuit", getActiveToolLabel());
 	}
 
+	private String getActiveToolLabel() {
+		return  editor.forTestGetEditDomain().getPaletteViewer().getActiveTool().getLabel();
+	}
+	
+	
+	public void testGetEditPartWithLabelOnCanvas() throws Exception {
+		editor.activateTool("Label");
+		editor.mouseMoveLeftClick(10, 10);
+		SWTBotGefEditPart botPart = editor.getEditPart("Label");
+		assertNotNull(botPart);
+		assertTrue(botPart.part() instanceof LogicLabelEditPart);
+	}
+	
 
+	public void testGetEditPartWithLabelInsideNode() throws Exception {	
+		editor.activateTool("Circuit");
+		editor.mouseMoveLeftClick(10, 10);		
+		editor.activateTool("Label");
+		editor.mouseMoveLeftClick(10 + 3, 10 + 3);
+		
+		SWTBotGefEditPart botPart = editor.getEditPart("Label");
+		assertNotNull(botPart);
+		assertTrue(botPart.part() instanceof LogicLabelEditPart);
+	}	
+	
 
+	public void testDrag() throws Exception {
+		editor.activateTool("Label");
+		editor.mouseMoveLeftClick(10, 10);				
+		editor.mouseDrag("Label", 100, 110);
+		
+		Rectangle bounds = ((GraphicalEditPart) editor.getEditPart("Label").part()).getFigure().getBounds();
+		assertEquals(100, bounds.x);
+		assertEquals(110, bounds.y);
+	}
+	
+	public void testDirectEdit() throws Exception {
+		//TODO
+	}
 
+	
+	
 }
