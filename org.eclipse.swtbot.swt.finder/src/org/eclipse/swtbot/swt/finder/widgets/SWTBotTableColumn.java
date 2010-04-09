a9e19a5f9306b7aae6fe8b8dfd659313ac0cb0cb
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2010-04-09 01:17:25 +0000

    RESOLVED - 308537: SWTBotTableColumn should have a 1-arg constructor https://bugs.eclipse.org/bugs/show_bug.cgi?id=308537

--- org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/widgets/SWTBotTableColumn.java
+++ org.eclipse.swtbot.swt.finder/src/org/eclipse/swtbot/swt/finder/widgets/SWTBotTableColumn.java
@@ -14,6 +14,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
+import org.eclipse.swtbot.swt.finder.results.WidgetResult;
 import org.hamcrest.SelfDescribing;
 
 /**
@@ -24,6 +26,21 @@ public class SWTBotTableColumn extends AbstractSWTBot<TableColumn> {
 
 	private final Table	parent;
 
+	/**
+	 * Constructs a new instance of this object.
+	 * 
+	 * @param w the widget.
+	 * @throws WidgetNotFoundException if the widget is <code>null</code> or widget has been disposed.
+	 * @since 2.0
+	 */
+	public SWTBotTableColumn(final TableColumn w) throws WidgetNotFoundException {
+		this(w, UIThreadRunnable.syncExec(new WidgetResult<Table>() {
+			public Table run() {
+				return w.getParent();
+			}
+		}));
+	}
+	
 	/**
 	 * Constructs a new instance of this object.
 	 * 
