fdf10120665c5c91c8ec53cf64610ab9783fdd20
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2010-06-24 04:53:53 +0000

    Removed unused import.

--- org.eclipse.swtbot.junit4_x/src/org/eclipse/swtbot/swt/finder/junit/ScreenshotCaptureListener.java
+++ org.eclipse.swtbot.junit4_x/src/org/eclipse/swtbot/swt/finder/junit/ScreenshotCaptureListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 SWTBot Committers and others.
+ * Copyright (c) 2010 SWTBot Committers and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,8 +9,6 @@
  *     Hans Schwaebli - initial API and implementation (Bug 259787)
  *******************************************************************************/
 package org.eclipse.swtbot.swt.finder.junit;
-
-import java.io.File;
 
 import org.apache.log4j.Logger;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
