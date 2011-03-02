d5605f4f95cffd7c77bad3d9474c45edc0c29608
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2011-03-01 22:26:16 -0800

    Add a test suite for the forms plugin.

---
+++ org.eclipse.swtbot.forms.finder.test/src/org/eclipse/swtbot/forms/finder/test/AllTests.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Ketan Padegaonkar and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Ketan Padegaonkar - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swtbot.forms.finder.test;
+
+import org.eclipse.swtbot.forms.finder.test.widgets.*;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJUnit4Suite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Ketan Padegaonkar &lt;KetanPadegaonkar [at] gmail [dot] com&gt;
+ */
+@RunWith(SWTBotJUnit4Suite.class)
+@SuiteClasses({
+	SWTBotExpandableCompositeTest.class,
+	SWTBotFormTextTest.class,
+	SWTBotHyperlinkTest.class,
+	SWTBotImageHyperlinkTest.class,
+	SWTBotScrolledFormTextTest.class,
+	SWTBotSectionTest.class,
+	SWTBotTreeNodeTest.class,
+	SWTBotTwistieTest.class
+	})
+public class AllTests {
+
+}
